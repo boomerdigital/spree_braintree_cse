435f948bc8b374e9d346a61a98a671489d84a9b6
Eric Anderson <eric@pixelwareinc.com>
2015-07-30 13:38:41 -0400

    Cleanup excessive abstractions.
    The monkey-patch into the Braintree gateway was causing reloading errors.
    When I looked at the monkey-patch getting the problem I didn't really
    understand the need for much of it (and I think I wrote it!). So removed it
    and simplified. After doing that the reloading error when away.

--- app/models/spree/gateway/braintree_gateway_decorator.rb
+++ app/models/spree/gateway/braintree_gateway_decorator.rb
@@ -1,41 +1,36 @@
-module Spree
-  class Gateway::BraintreeGateway
-    concerning :CSE do
-      included do
-        unless method_defined? :options_for_payment_without_cse
-          preference :use_client_side_encryption, :boolean
-          alias_method_chain :authorize, :cse
-          alias_method_chain :options_for_payment, :cse
-        end
-      end
+Spree::Gateway::BraintreeGateway.class_eval do
 
-      def authorize_with_cse(money, creditcard, options = {})
-        if creditcard.gateway_payment_profile_id && !creditcard.gateway_customer_profile_id && preferred_use_client_side_encryption
-          payment_method = creditcard.gateway_payment_profile_id
-          options[:payment_method_nonce] = true
-          provider.authorize(money, payment_method, options)
-        else
-          authorize_without_cse(money, creditcard, options)
-        end
-      end
-
-      def method_type
-        if preferred_use_client_side_encryption
-          "braintree"
-        else
-          super
-        end
-      end
-
-      def options_for_payment_with_cse(p)
-        if p.source.gateway_customer_profile_id.blank? && p.source.gateway_payment_profile_id.blank? && p.source.encrypted_data.present?
-          options = options_for_payment_without_cse(p)
-          options.merge!({ payment_method_nonce: p.source.encrypted_data })
-        else
-          options_for_payment_without_cse(p)
-        end
-      end
-
+  def authorize_with_cse(money, creditcard, options = {})
+    if creditcard.gateway_payment_profile_id && !creditcard.gateway_customer_profile_id && preferred_use_client_side_encryption
+      payment_method = creditcard.gateway_payment_profile_id
+      options[:payment_method_nonce] = true
+      provider.authorize(money, payment_method, options)
+    else
+      authorize_without_cse(money, creditcard, options)
     end
   end
+
+  def method_type
+    if preferred_use_client_side_encryption
+      "braintree"
+    else
+      super
+    end
+  end
+
+  def options_for_payment_with_cse(p)
+    if p.source.gateway_customer_profile_id.blank? && p.source.gateway_payment_profile_id.blank? && p.source.encrypted_data.present?
+      options = options_for_payment_without_cse(p)
+      options.merge!({ payment_method_nonce: p.source.encrypted_data })
+    else
+      options_for_payment_without_cse(p)
+    end
+  end
+
+  unless method_defined? :options_for_payment_without_cse
+    preference :use_client_side_encryption, :boolean
+    alias_method_chain :authorize, :cse
+    alias_method_chain :options_for_payment, :cse
+  end
+
 end
