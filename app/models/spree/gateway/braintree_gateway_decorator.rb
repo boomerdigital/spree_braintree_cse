a1faee0bf4d4d0a02140d40e4f41567cfccb9a2a
Eric Anderson <eric@pixelwareinc.com>
2016-01-14 11:34:23 -0500

    Refactor overrides to use prepend.
    The alias_method_chain doesn't mix will with the `decorator` pattern
    found in Spree apps. `prepend` is re-entrant (when using a named module)
    and relies on inheritance instead of method renaming resulting in a
    cleaner way to do this type of override.

--- app/models/spree/gateway/braintree_gateway_decorator.rb
+++ app/models/spree/gateway/braintree_gateway_decorator.rb
@@ -1,34 +1,32 @@
 Spree::Gateway::BraintreeGateway.class_eval do
-  def authorize_with_cse(money, creditcard, options = {})
-    if creditcard.gateway_payment_profile_id && !creditcard.gateway_customer_profile_id && preferred_use_client_side_encryption
-      payment_method = creditcard.gateway_payment_profile_id
-      options[:payment_method_nonce] = true
-      provider.authorize(money, payment_method, options)
-    else
-      authorize_without_cse(money, creditcard, options)
+  preference :use_client_side_encryption, :boolean
+
+  module CSE
+    def authorize money, creditcard, options = {}
+      if creditcard.gateway_payment_profile_id && !creditcard.gateway_customer_profile_id && preferred_use_client_side_encryption
+        payment_method = creditcard.gateway_payment_profile_id
+        options[:payment_method_nonce] = true
+        provider.authorize money, payment_method, options
+      else
+        super
+      end
+    end
+
+    def method_type
+      if preferred_use_client_side_encryption
+        'braintree'
+      else
+        super
+      end
+    end
+
+    def options_for_payment payment
+      if payment.source.gateway_customer_profile_id.blank? && payment.source.gateway_payment_profile_id.blank? && payment.source.encrypted_data.present?
+        super.merge payment_method_nonce: payment.source.encrypted_data
+      else
+        super
+      end
     end
   end
-
-  def method_type
-    if preferred_use_client_side_encryption
-      'braintree'
-    else
-      super
-    end
-  end
-
-  def options_for_payment_with_cse(p)
-    if p.source.gateway_customer_profile_id.blank? && p.source.gateway_payment_profile_id.blank? && p.source.encrypted_data.present?
-      options = options_for_payment_without_cse(p)
-      options.merge!({ payment_method_nonce: p.source.encrypted_data })
-    else
-      options_for_payment_without_cse(p)
-    end
-  end
-
-  unless method_defined? :options_for_payment_without_cse
-    preference :use_client_side_encryption, :boolean
-    alias_method_chain :authorize, :cse
-    alias_method_chain :options_for_payment, :cse
-  end
+  prepend CSE
 end
