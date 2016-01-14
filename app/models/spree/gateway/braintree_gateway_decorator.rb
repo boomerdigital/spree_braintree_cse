c4745cfc8b97629222edcb9af4769c8d4e920641
Eric Anderson <eric@pixelwareinc.com>
2016-01-14 12:10:32 -0500

    Make intention of conditionals more clear.
    The previous code was a little non-obvious regarding the purpose of those
    long conditionals. Broke them up into intention-reveling methods to make
    it more obvious and more readable.

--- app/models/spree/gateway/braintree_gateway_decorator.rb
+++ app/models/spree/gateway/braintree_gateway_decorator.rb
@@ -2,9 +2,9 @@ Spree::Gateway::BraintreeGateway.class_eval do
   preference :use_client_side_encryption, :boolean
 
   module CSE
-    def authorize money, creditcard, options = {}
-      if creditcard.gateway_payment_profile_id && !creditcard.gateway_customer_profile_id && preferred_use_client_side_encryption
-        payment_method = creditcard.gateway_payment_profile_id
+    def authorize money, credit_card, options = {}
+      if using_cse? && using_saved_card?(credit_card)
+        payment_method = credit_card.gateway_payment_profile_id
         options[:payment_method_nonce] = true
         provider.authorize money, payment_method, options
       else
@@ -13,7 +13,7 @@ Spree::Gateway::BraintreeGateway.class_eval do
     end
 
     def method_type
-      if preferred_use_client_side_encryption
+      if using_cse?
         'braintree'
       else
         super
@@ -21,11 +21,34 @@ Spree::Gateway::BraintreeGateway.class_eval do
     end
 
     def options_for_payment payment
-      if payment.source.gateway_customer_profile_id.blank? && payment.source.gateway_payment_profile_id.blank? && payment.source.encrypted_data.present?
+      if payment.source.encrypted_data.present? && no_saved_card?(payment.source)
         super.merge payment_method_nonce: payment.source.encrypted_data
       else
         super
       end
+    end
+
+    private
+
+    def no_saved_card? credit_card
+      !customer_profile?(credit_card) && !payment_profile?(credit_card)
+    end
+
+    def using_saved_card? credit_card
+      payment_profile?(credit_card) && !customer_profile?(credit_card)
+    end
+
+    def customer_profile? credit_card
+      credit_card.gateway_customer_profile_id.present?
+    end
+
+    def payment_profile? credit_card
+      credit_card.gateway_payment_profile_id.present?
+    end
+
+    # A bit less verbose
+    def using_cse?
+      preferred_use_client_side_encryption
     end
   end
   prepend CSE
